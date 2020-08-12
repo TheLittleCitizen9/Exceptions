4fb8faa7965ff3a83a7e51c34e3fe639d79df97e
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-12 11:36:18 +0300

    Added ScubaException

---
+++ ExceptionsAndErrorHandling/ExceptionsAndErrorHandling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExceptionsAndErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+
+            try
+            {
+                throw new ScubaException(number);
+            }
+            catch (ScubaException e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ ExceptionsAndErrorHandling/ExceptionsAndErrorHandling/ScubaException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsAndErrorHandling
+{
+    public class ScubaException : Exception
+    {
+        const string MESSAGE = "This is a Scuba Exception";
+        private int _studentNumber;
+        public ScubaException(int studentNumber):base(MESSAGE)
+        {
+            _studentNumber = studentNumber;
+
+
+        }
+    }
+}
